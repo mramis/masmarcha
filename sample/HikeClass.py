5e9df22ba6e1aa26aac50120be381efd62d1cc79
Mariano <ramismariano@gmail.com>
2017-10-22 20:47:23 -0300

    Se modifica la clase Hike para que acepte esquemas de marcadores. Todavía falta el test.

--- sample/HikeClass.py
+++ sample/HikeClass.py
@@ -2,9 +2,9 @@
 # coding: utf-8
 
 """La nueva clase Hike (antigua clase Trayectoria), se encarga de recibir
-los datos de los marcadores, de agruparlos correctamente, y de corregir la
-falta de datos. El trabajo de establecer los ciclos los va a realizar la
-clase Kinematic.
+los datos de los marcadores, de agruparlos correctamente, de corregir la
+falta de datos, de definir los ciclos, y calcular los angulos dentro del
+ciclo. Tambien los presenta como Pandas DataFrame.
 """
 
 # Copyright (C) 2017  Mariano Ramis
@@ -35,7 +35,8 @@ from video.proccess import (interval, interp, homogenize, positiveX, negativeX,
 class Hike(object):
     """Conserva los datos de los marcadores de la caminata en un sentido."""
 
-    def __init__(self, fps, metrics):
+    def __init__(self, schema, fps, metrics):
+        self.schema = schema
         self.start_videoframe_position = None
         self.end_videoframe_position = None
         self.direction = None
@@ -43,40 +44,43 @@ class Hike(object):
         self.videofps = fps
         self.videometricsample = metrics[0]  # muestra de referencia
         self.videometricscale = metrics[1]  # escala de conversión
-        self._groups_markers = {0: [], 1: [], 2: [], 'index_order': []}
-        self._filled_groups = False
-        self._fixed_groups = False
-        self._to_interpolate = {0: [], 1: [], 2: []}
+        self._gcluster = {g: [] for g in schema['i.groups']}
+        self._gcluster['index_order'] = []
+        self._to_interpolate = {g: [] for g in schema['i.groups']}
+        self._filled_cluster = False
+        self._fixed_cluster = False
         self._count_frames = 0
         self._cycled = None
+        self._sorted = None
+        self._interpolated = None
 
-    def add_markers_from_videoframe(self, index, groups):
+    def add_markers_from_videoframe(self, index, cluster):
         u"""Se agregan las trayectorias de los marcadores.
 
         :param index: índice del cuadro de video.
         :type index: int
-        :param groups: (G0, G1, G2), arreglo de los grupos de marcadores.
-        :type groups: tuple
+        :param cluster: (G0, G1, G2), arreglo de los grupos de marcadores.
+        :type cluster: tuple
         """
         self._count_frames += 1
-        for i, g in zip((0, 1, 2), groups):
-            self._groups_markers[i].append(g)
-        self._groups_markers['index_order'].append(index)
+        for i, g in zip(self.schema['i.groups'], cluster):
+            self._gcluster[i].append(g)
+        self._gcluster['index_order'].append(index)
 
-    def add_index_to_interpolate(self, index, bgroups):
-        u"""Se agregan indices que tienen que ser interpolados.
+    def add_index_to_interpolate(self, index, bcluster):
+        u"""Se agregan indices de cuadros a interpolar.
 
         :param index: índice del cuadro de video.
         :type index: int
-        :param bgroups: lista de booleanos, que indica los grupos que deben ser
-        interpolados.
-        :type bgroups: list or tuple
+        :param bcluster: lista de booleanos, que indica los grupos que deben
+        ser interpolados.
+        :type bcluster: list or tuple
         """
         self._count_frames += 1
-        for i, bg in zip((0, 1, 2), bgroups):
+        for i, bg in zip(self.schema['i.groups'], bcluster):
             self._to_interpolate[i].append((index, bg))
 
-    def rm_last_data_added(self, n):
+    def rm_last_added_data(self, n):
         u"""Remueve los últimos n datos de marcadores añadidos.
 
         Este método se aplica al finalizar la adición de datos de marcadores.
@@ -85,17 +89,38 @@ class Hike(object):
         """
         self._count_frames -= n
         while n:
-            self._groups_markers[0].pop()
-            self._groups_markers[1].pop()
-            self._groups_markers[2].pop()
-            self._groups_markers['index_order'].pop()
-            self._to_interpolate[0].pop()
-            self._to_interpolate[1].pop()
-            self._to_interpolate[2].pop()
+            for i in self.schema['i.groups']:
+                self._gcluster[i].pop()
+                self._to_interpolate[i].pop()
+            self._gcluster['index_order'].pop()
             n -= 1
 
-    def get_foot_direction(self, mode=0, ret=False):
-        u"""Establece la dirección del pie.
+    def fill_groups(self):
+        u"""Rellenar grupos.
+
+        Este método toma cada conjunto de marcadores por cuadro, y cada grupo
+        por conjunto. Entonces revisa que el número de marcadores que existe en
+        el conjunto sea igual al número esperado *según el esquema*. Si no lo
+        es, reemplaza el grupo de marcadores por un arreglo de ceros, con la
+        misma dimensión que la esperada.
+        Cabe mencionar que el tipo de arreglo de grupo es un arreglo de Numpy.
+        """
+        if not self._filled_cluster:
+            filled_cluster = {}
+            markers = []
+            iterable = zip(self.schema['n.markers'], self.schema['i.groups'])
+            for nm, group in iterable:
+                for arr in self._gcluster[group]:
+                    if arr.shape[0] == nm:
+                        markers.append(arr)
+                    else:
+                        markers.append(np.zeros((nm, 2)))
+                filled_cluster[group] = np.array(markers)
+                markers = []
+            self._filled_cluster = filled_cluster
+
+    def get_direction(self, force_mod=False):
+        u"""Establece la dirección de la caminata.
 
         A través del cálculo diferencial de las componentes en "y" del marcador
         de tobillo(G2) establece la velocidad de movimiento del pie. Luego
@@ -103,107 +128,70 @@ class Hike(object):
         de apoyo completo.
         :return: None
         """
-        if self.direction:
-            if ret:
-                return self.direction
+        if not self.direction:
+            mod = self.schema['d.mod']
+            if force_mod:
+                mod = force_mod
+            self.fill_cluster()
+            self.sort_foot()
+
+            direction = 0
+            arr = self._filled_cluster[self.schema['gr.foot']]
+            if mod:
+                ydiff = np.diff(arr[:, self.schema['mk.ankle'], 1])
+                ffoot, rfoot, __ = self.schema['or.foot']
+                for n, diff in enumerate(ydiff):
+                    if diff == 0:
+                        X1 = arr[:, rfoot, 0][n]
+                        X0 = arr[:, ffoot, 0][n]
+                        direction += X1 - X0
             else:
-                return
-        if not self._filled_groups:
-            self.fill_groups()
+                X0 = arr[:, 0, 0][0]
+                X1 = arr[:, 0, 0][-1]
+                direction = X1 - X0
 
-        direction = 0
-        arr = np.array(self._filled_groups[2])
-        if mode:
-            ydiff = np.diff(arr[:, 2, 1])
-            for n, diff in enumerate(ydiff):
-                if diff == 0:
-                    XA = arr[:, 2, 0][n]
-                    X0 = arr[:, 0, 0][n]
-                    X1 = arr[:, 1, 0][n]
-                    if abs(XA - X0) < abs(XA - X1):  # Entonces X0 es pp
-                        direction += X1-X0
-                    else:  # Entonces X1 es pp
-                        direction += X0-X1
-        else:
-            X0 = arr[:, 2, 0][0]
-            X1 = arr[:, 2, 0][-1]
-            direction = X1 - X0
-        self.direction = 1 if direction > 0 else -1
-        if ret:
-            return self.direction
+            self.direction = 1 if direction > 0 else -1
 
-    def fill_groups(self):
-        u"""."""
-        if not self._filled_groups:
-            filled_groups = {}
-            markers = []
-            for nm, group in zip((2, 2, 3), (0, 1, 2)):
-                for arr in self._groups_markers[group]:
-                    if arr.shape[0] == nm:
-                        markers.append(arr)
-                    else:
-                        markers.append(np.zeros((nm, 2)))
-                filled_groups[group] = markers
-                markers = []
-            self._filled_groups = filled_groups
-
-    def markers_as_dataframe(self, wich_groups='fixed'):
-        u"""."""
-        markers_group = {
-            'filled': self._filled_groups,
-            'fixed': self._fixed_groups
-        }
-        if wich_groups not in markers_group.keys():
-            print '#mm: Bad group. Options are: filled, fixed.'
-            return
-        self.fill_groups()
-        self.fix_groups()
-
-        df = []
-        rows = product(('fs', 'tri', 'ts', 'fi', 'pa', 'pp', 'ti'), ('x', 'y'))
-        ix = pd.MultiIndex.from_tuples(tuple(rows), names=['markers', 'coord'])
-        for group, codes in zip((0, 1, 2), (2, 2, 3)):
-            arr = np.array(markers_group[wich_groups][group])
-            for i in xrange(codes):  # rows, markers, columns
-                df.append(arr[:, i, 0])
-                df.append(arr[:, i, 1])
-        return pd.DataFrame(df, index=ix, dtype='int').replace(0, np.nan)
-
-    def fix_groups(self):
-        u"""Corrige el orden de los marcadores de tobillo e interpola datos."""
-        if not self._fixed_groups:
-            if not self.direction:
-                self.get_foot_direction()
-            if not self._filled_groups:
-                self.fill_groups()
-            self._sort_foot()
-            self._interpolate()
-
-    def _sort_foot(self):
+    def sort_foot(self):
         u"""Ordena los marcadores del grupo de tobillo."""
-        G2copy = np.array([arr for arr in self._filled_groups[2]])
-        D0 = np.linalg.norm(G2copy[:, 2, :] - G2copy[:, 0, :], axis=1)
-        D1 = np.linalg.norm(G2copy[:, 2, :] - G2copy[:, 1, :], axis=1)
-        for i, mindst in enumerate(D0 < D1):
-            M0, M1, M2 = G2copy[i]
-            if mindst:
-                G2copy[i] = np.array((M1, M0, M2))
-        self._fixed_groups = self._filled_groups.copy()
-        self._fixed_groups[2] = G2copy
+        if self.schema['sort_foot'] and not self._sorted:
+            foot_group = self._filled_cluster[self.schema['gr.foot']].copy()
+            ffoot, rfoot, ankle = self.schema['or.foot']
+            ankle_rfoot = foot_group[:, ankle, :] - foot_group[:, rfoot, :]
+            ankle_ffoot = foot_group[:, ankle, :] - foot_group[:, ffoot, :]
+            d0 = np.linalg.norm(ankle_rfoot, axis=1)
+            d1 = np.linalg.norm(ankle_ffoot, axis=1)
+            for i, mindst in enumerate(d0 < d1):
+                m0, m1, m2 = foot_group[i]
+                if mindst:
+                    foot_group[i] = np.array((m1, m0, m2))
+            self._fixed_cluster = self._filled_cluster.copy()
+            self._fixed_cluster[self.schema['gr.foot']] = foot_group
+            self._sorted = True
 
-    def _interpolate(self):
+    def interpolate(self):
         u"""Realiza una interpolación lineal de los datos faltantes."""
-        for group in (0, 1, 2):
-            indexes = self._to_interpolate[group]
-            if indexes:
-                for ai, bi in interval([i for i, b in indexes if b is True]):
-                    ai -= self.start_videoframe_position
-                    bi -= self.start_videoframe_position
-                    dt = (bi - ai) - 1
-                    A = self._fixed_groups[group][ai]
-                    B = self._fixed_groups[group][bi]
-                    for j, arr in zip(xrange(ai + 1, bi), interp(A, B, dt)):
-                        self._fixed_groups[group][j] = arr
+        if self.schema['interpolate'] and not self._interpolated:
+            for group in self.schema['i.groups']:
+                indexes = self._to_interpolate[group]
+                if indexes:
+                    frames = [i for i, b in indexes if b is True]
+                    for ai, bi in interval(frames):
+                        ai -= self.start_videoframe_position
+                        bi -= self.start_videoframe_position
+                        dt = (bi - ai) - 1
+                        A = self._fixed_cluster[group][ai]
+                        B = self._fixed_cluster[group][bi]
+                        for j, arr in zip(range(ai + 1, bi), interp(A, B, dt)):
+                            self._fixed_cluster[group][j] = arr
+            self._interpolated = True
+
+    def fix_cluster(self):
+        u"""Corrige el orden de los marcadores de tobillo e interpola datos."""
+        if not self._fixed_cluster:
+            self.fill_cluster()
+            self.sort_foot()
+            self.interpolate()
 
     def cycle_definition(self, cycle_level=.15):
         u"""Definición de ciclo.
@@ -215,12 +203,11 @@ class Hike(object):
         movimiento.
         :type level: float
         """
-        if not self._fixed_groups:
-            self.fix_groups()
+        self.fix_cluster()
 
         mov = []
-        for i in (0, 1):
-            pi = np.array(self._fixed_groups[2])[:, i, :]
+        for i in self.schema['mk.foot']:
+            pi = self._fixed_cluster[self.schema['gr.foot']][:, i, :]
             pmv = np.abs(np.gradient(pi)[0].sum(axis=1))
             pmv[pmv < max(pmv)*cycle_level] = 0
             homogenize(pmv)
@@ -245,16 +232,17 @@ class Hike(object):
             cycles = np.array(cycles)
             # NOTE: Parámetros espaciotemporales. Zancada y tiempos.
             ihs, to, ehs = cycles.transpose()
-            X0 = np.array(self._fixed_groups[2])[ihs, 2, :]
-            X1 = np.array(self._fixed_groups[2])[ehs, 2, :]
+            ankle = self.schema['mk.ankle']
+            X0 = self._fixed_cluster[self.schema['gr.foot']][ihs, ankle, :]
+            X1 = self._fixed_cluster[self.schema['gr.foot']][ehs, ankle, :]
             self.pxstride = np.linalg.norm(X0 - X1, axis=1)
             self.stride = px_to_m(self.pxstride,
                                   self.videometricsample,
                                   self.videometricscale)
-            self.times = np.array([(ehs - ihs),
-                                   (to - ihs),
-                                   (ehs - to)],
-                                  dtype='float') / self.videofps
+            times_array = np.array(
+                ((ehs - ihs), (to - ihs), (ehs - to)), dtype='float'
+            )
+            self.times = times_array / self.videofps
             self.phases = self.times[1:] / self.times[0]
             self.cycles = cycles
             self.ncycles = len(cycles)
@@ -266,37 +254,59 @@ class Hike(object):
     def joints_definition(self):
         u"""."""
 
-        self.fix_groups()
+        self.fix_cluster()
         self.cycle_definition()
-        if self.cycles is None:
+        if not self.cycles:
             return
 
         for i, cycle in enumerate(self.cycles):
             ihs, to, ehs = cycle
             markers = {}
-            codenames = (('tr', 'fs'), ('ts', 'fi'), ('pa', 'pp', 'ti'))
-            for j in (0, 1, 2):
-                group = np.array(self._fixed_groups[j])
-                for k, name in enumerate(codenames[j]):  # rows,markers,columns
-                    markers[name] = group[ihs: ehs+1, k, :]
-            # NOTE: El marcador de tronco no se está utilizando.
-            tight = markers['fi'] - markers['fs']
-            leg = markers['ti'] - markers['ts']
-            foot = markers['pp'] - markers['pa']
+            for j in self.schema['i.groups']:
+                for k, name in enumerate(self.schema['mk.codenames'][j]):
+                    markers[name] = self._fixed_cluster[j][ihs: ehs+1, k, :]
+            segments = {}
+            for segment in self.schema['or.segments']:
+                key = 'sg.%s' % segment
+                a, b = self.schema[key]
+                segments[segment] = markers[a] - markers[b]
 
-            xdirection = (None, positiveX, negativeX)[self.direction]
-            hip = 90 - angle(tight, xdirection(tight.shape[0]))
-            knee = hip + angle(leg, xdirection(leg.shape[0])) - 90
-            ankle = 90 - angle(leg, foot)
-            hip, knee, ankle = fourierfit(np.array((hip, knee, ankle)))
-            self.joints[i] = {'hip': hip, 'knee': knee, 'ankle': ankle}
+            if not self.direction:
+                self.get_direction()
+
+            joints = Joints(self.schema, segments, self.direction)
+            self.joints[i] = joints.fit()
+
+    def markers_as_dataframe(self, wich_cluster='fixed'):
+        u"""."""
+        markers_group = {
+            'filled': self._filled_cluster,
+            'fixed': self._fixed_cluster
+        }
+        if wich_cluster not in markers_group.keys():
+            print '#mm: Bad group. Options are: filled, fixed.'
+            return
+
+        self.fill_cluster()
+        self.fix_cluster()
+        df = []
+        markers_name = reduce(lambda x, y: x + y, self.schema['mk.codenames'])
+        rows = (markers_name, ('x', 'y'))
+        ix = pd.MultiIndex.from_product(rows, names=['markers', 'coord'])
+        iterable = zip(self.schema['i.groups'], self.schema['n.markers'])
+        for group, codes in iterable:
+            arr = np.array(markers_group[wich_cluster][group])
+            for i in xrange(codes):
+                df.append(arr[:, i, 0])
+                df.append(arr[:, i, 1])
+        return pd.DataFrame(df, index=ix, dtype='int').replace(0, np.nan)
 
     def joints_as_dataframe(self, code=''):
         u"""."""
         if self._cycled:
             dataframe = []
             codecycles = [code + str(i) for i in xrange(self.ncycles)]
-            joints = ('hip', 'knee', 'ankle')
+            joints = self.schema['or.joints']
             ix = pd.MultiIndex.from_product(
                 (codecycles, joints),
                 names=('cycle', 'joint')
@@ -305,6 +315,8 @@ class Hike(object):
                 for joint in joints:
                     dataframe.append(self.joints[cycle][joint])
             return pd.DataFrame(dataframe, index=ix)
+        else:
+            print '#mm: not cycled yet'
 
     def spatiotemporal_as_dataframe(self, code=''):
         u"""."""
@@ -320,3 +332,49 @@ class Hike(object):
                                    120 / self.times[0]
                                    ))
             return pd.DataFrame(dataframe, index=ix, columns=codecycles)
+        else:
+            print '#mm: not cycled yet'
+
+
+class Joints(object):
+
+    def __init__(self, schema, segments, direction):
+        self.schema = schema
+        self.segments = segments
+        self.xcanonical = (None, positiveX, negativeX)
+        self.direction = direction
+        self.joints = {j: None for j in schema['or.joints']}
+
+    def get_hip(self):
+        if not self.joints['hip']:
+            canonical = self.xcanonical[self.direction]
+            tight = self.segments['tight']
+            self.joints['hip'] = 90 - angle(tight, canonical(tight.shape[0]))
+        return self.joints['hip']
+
+    def get_knee(self):
+        if not self.joints['knee']:
+            canonical = self.xcanonical[self.direction]
+            leg = self.segments['leg']
+            if not self.hip:
+                hip = self.get_hip()
+            self.joints['knee'] = hip + angle(leg, canonical(leg.shape[0])) - 90
+        return self.joints['knee']
+
+    def get_ankle(self):
+        if not self.joints['ankle']:
+            leg = self.segments['leg']
+            foot = self.segments['foot']
+            self.joints['ankle'] = 90 - angle(leg, foot)
+        return self.joints['ankle']
+
+    def fit(self):
+        method = {
+            'hip': "self.get_hip()",
+            'knee': "self.get_knee()",
+            'ankle': "self.get_ankle()",
+        }
+        joints = self.schema['or.joints']
+        to_fit = [eval(method[joint]) for joint in joints]
+        self.joints = {j: f for j, f in zip(joints, fourierfit(to_fit))}
+        return self.joints
