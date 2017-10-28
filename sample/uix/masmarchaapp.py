809642f4f52662a11fe01a6dcd3dc26ed87b5bcf
Mariano <ramismariano@gmail.com>
2017-10-28 18:41:20 -0300

    Desarrollando interfaz de usuario.

---
+++ sample/uix/masmarchaapp.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""En construcción."""
+
+# Copyright (C) 2017  Mariano Ramis
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from kivy.app import App
+from files import Content
+
+
+class MasMarchaApp(App):
+    u"""."""
+
+    def build(self):
+        u"""."""
+        return Content()
+
+
+if __name__ == '__main__':
+    MasMarchaApp().run()
