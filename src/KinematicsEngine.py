dd9a56a70c7930862d1648d53cb7a76c2673940c
Mariano <ramismariano@gmail.com>
2018-02-28 21:49:43 -0300

    Se sigue trabajando en el motor de lectura de datos de marcha. Se finaliza con el código de identificar marcadores, pero resta probarlo en un intervalo de video.

--- src/KinematicsEngine.py
+++ src/KinematicsEngine.py
@@ -4,7 +4,7 @@
 # In[1]:
 
 
-get_ipython().magic(u'matplotlib inline')
+# %matplotlib inline
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
@@ -79,132 +79,235 @@ def sliceHikes(buff, container, Nframes, expected_num):
                 backward += 1
 
 
-# In[17]:
+# In[ ]:
 
 
-# Este es bloque es está en desarrollo!
+# Esquemas:!
 
-def centroid(x, y, w, h):
-    u"""Calula el centro de una caja.
+global schema
+# Nuevo esquema
+# schema = {
+#     'schema': (3, 2, 3),
+#     'codes': (('M0', 'M1', 'M2'), ('M3', 'M4'), ('M5', 'M6', 'M7')),    
+# }
 
-    :param x, y: Vértice superior izquierdo.
-    :param w, h: Ancho y Alto de la caja.
-    """
+#Anterior esquema
+schema = {
+    'schema': (2, 2, 3),
+    'codes': (('M0', 'M1'), ('M2', 'M3'), ('M4', 'M5', 'M6')),
+}
+
+#Viejo esquema 
+# schema = {
+#     'schema': (1, 1, 2),
+#     'codes': (('M0'), ('M1'), ('M2', 'M3')),   
+# }
+
+
+# In[13]:
+
+
+# OK!
+
+def markerCenter(contour):
+    u"""Devuelve los centros de los contorno del marcador."""
+    x, y, w, h = cv2.boundingRect(contour)
     xc = x + w/2
     yc = y + h/2
     return xc, yc
 
 
-def center_of_square(contour):
-    u"""Centro de cuadrado.
-
-    Devuelve el centro del cuadrado que encierra los marcadores de la image.
-    :param contour: objeto contorno del marcador.
-    :type contour: cv2.findContours
-    :return: int, int: (x, y) centro del contorno.
-    :rtype: tuple
-    """
-    return centroid(*cv2.boundingRect(contour))
-
-
-def roi_center(array, amp=1.4):
-    u"""Región de interes.
-
-    Calcula límites de región según el arreglo de posiciones y la amplitud que
-    se pasa en el argumento. Toma las fronteras verticales del arreglo y los
-    amplia según el factor amplitud.
-    :param array: grupo de centros de marcadores(posiciones).
-    :type array: np.ndarray
-    :return: int, int: (ya, yb) los limites superior e inferior del grupo,
-    ampliados.
-    :rtype: tuple
-    """
-    y1 = array[0][1]
-    y2 = array[-1][1]
-    dy = (y1 - y2)*amp
-    center = (y1 + y2) / 2
-    return(center - dy, center + dy)
-
-
-# def knee_roi_center(array, age, amp=1.4):  # FUTURO: Cuando tengamos video con marcadores en vertex.
-#     u"""Calcula el centro de la región de interés de.
-
-#     Calcula límites de región según el arreglo de posiciones y la amplitud que
-#     se pasa en el argumento. Toma las fronteras verticales del arreglo y los
-#     amplia según el factor amplitud.
-#     :param array: grupo de centros de marcadores(posiciones).
-#     :type array: np.ndarray
-#     :return: int, int: (ya, yb) los limites superior e inferior del grupo,
-#     ampliados.
-#     :rtype: tuple
-#     """
-#     height_div = {'child': (6.0, 5.5), 'adult': (8.0, 6.5)}[age]
-#     ytop = array[0][1]
-#     ybottom = array[-1][1]
-#     dy = (ybottom - ytop) / height_div[0]
-#     center = ytop - dy * height_div[1]
-#     return(center - dy, center + dy)
-
-
-
-def grouping(markers, n_expected, kroi=False):
-    u"""Agrupamiento.
-
-    Esta función agrupa los centros de marcadores, con el objetivo de disminuir
-    los datos que se pierden y por tanto, los que tienen que ser interpolados.
-    Los tres grupos son G0: los marcadores de tronco y muslo superior, G1: los
-    marcadores de la región de rodilla, y G2: los marcadores de la región de
-    tobillo.
-    La función acepta como argumento opcional la zona de interes de la rodilla
-    (ver roi_center) que establece los grupos cuando el número de marcadores es
-    distinto al esperado.
-    :param markers: marcadores(centros) obtenidos del proceso del videoframe.
-    :type markers: np.ndarray
-    :param n_expected: (int, int, int): arreglo con el número de marcadores
-    esperados por grupos.
-    :type n_expected: tuple
-    :return: (list, tuple): lista de booleanos que indica los grupos que tienen
-    que ser interpolados, y tupla con los grupos (G0, G1, G2) siendo Gi un
-    np.ndarray.
-    :rtype: tuple
-    """
-    if kroi:
-        Y = markers[:, 1]
-        upp = markers[Y < kroi[0]]
-        mid = markers[np.logical_and(Y > kroi[0], Y < kroi[1])]
-        low = markers[Y > kroi[1]]
-    else:
-        upp = markers[5:, :]
-        mid = markers[3:5, :]
-        low = markers[:3, :]
-    n_obtained = (upp.shape[0], mid.shape[0], low.shape[0])
-    boolean_interpolate = [a != b for a, b in zip(n_expected, n_obtained)]
-    return(boolean_interpolate, (G0, G1, G2))
-
-                
-def groupMarkers(buff, container, Nframes, expected_num):
-    u"""DESARROLLANDO."""
+def identifyingMarkers(buff, Nframes, schema, **kwargs):
+    u"""Se obtienen e identifican los arreglos de marcadores en el segmento de video.
     
+    Esta función extrae los centros de marcadores ordenados en sentido descendente,
+    de la forma en la que está diagramado el esquema de los mismos. La función
+    también identifica los cudros y las regiones dentro del cuadro donde faltan
+    datos, es decir, el número de marcadores que se encuentra no es el esperado.
+    Devuelve un arreglo que contiene todos los marcadores del segmento de video que
+    se le pasa como argumento, junto a una lista de cuadros y regiones que deben ser
+    interpoladas.
+
+    :param buff: stream de video para ser analizado.
+    :type buff: cv2.VideoCapture
+    :param Nframes: el número de cuadros que contiene el segmento de video.
+    :type Nframes: int
+    :param schema: es el vector que contiene la información de la cantidad de
+    marcadores que contiene cada región.
+    :type schema: tuple
+    :return: arreglo con los arreglos de los centros del marcadores que se
+    encontraron en el fragmento de video. Lista con los indices de los cuadros
+    y las regiones de cada cuadro que contienen datos ausentes.
+    :rtype: tuple
+    """
+
+    markers = []
+    missing = []
     for i in xrange(Nframes):
+        # Se lee el cuadro y se extraen los centros de los marcadores en
+        # un arreglo de numpy.
         __, frame = buff.read()
-        contours = findMarkers(frame)
-        N = len(contours)
-        markers = np.array(map(center_of_square, contours))
-        if N == expected_num:
-            array = grouping(markers, expected_num)
-            kneezone = roi_center(markers)
+        centers = np.array(map(markerCenter, findMarkers(frame)))[::-1]
+        # Si el número de marcadores es el adecuado, entonces se almacena
+        # en una variable temporal el arreglo de centros por si es
+        # necesario en el siguiente cuadro rellenar un arreglo incompleto
+        if len(centers) == sum(schema):
+            # Se revisa el orden dentro de los marcadores de la región del
+            # pie, puesto que en la lectura hecha por OpenCV puede haberse
+            # invertido el orden de los marcadores de pie.
+            centers = sortFootMarkers(centers, schema)
+            temp = centers
         else:
-            array = grouping(markers, expected_num, kneezone)
-        print array
+            # Si el arreglo está incompleto entonces se rellena el arreglo
+            # en las regiones donde falten marcadores. En este proceso se
+            # buscan las regiones de los marcadores en el último arreglo
+            # completo.
+            roi = regions(temp, schema, **kwargs)
+            centers, invschema = filling(centers, roi, schema)
+            # se registran las regiones del cuadro donde faltaron marcadores
+            # para que posteriormente sea interpoladas esas regiones.
+            missing.append((i, invschema))
+        markers.append(centers)
+    return (np.array(markers), missing)
 
 
-# In[18]:
+def sortFootMarkers(centers, schema):
+    u"""Ordena los marcadores del grupo de tobillo."""
+    # Por diagrama de marcadores, los que se encuentran en el pie son los
+    # que están en la última región.
+    i , j = (sum(schema) - schema[-1], sum(schema))
+    # => foot = centers[i: j]
+    # Como los marcadores que están en la región de tobillo son tres, y el
+    # esquema dice que tienen que estar colocados céfalo-caudales, entonces
+    # el de tobillo es el antepenúltimo, el de la parte posterior del pie
+    # es el anteultimo, y el de la parte anterior del pié es el último.
+    ankle, rearf, frontf = centers[i: j] 
+    d0 = np.linalg.norm(ankle - rearf)
+    d1 = np.linalg.norm(ankle - frontf)
+    # Si la distancia entre el marcador de la parte posterior del pie y el tobillo
+    # es mayor que la distancia entre la parte anterior del pie y el tobillo,
+    # entonces el ciclo de la marcha se encuentra en un instante donde el marcador
+    # de la parte anterior del pie está por encima del marcador que se encuentra
+    # en la parte posterior del pie, y la función de encontrar los marcadores de
+    # OpenCV los ha ordenado de forma distinta al que se plantea en el esquema y
+    # se debe corregir.
+    if d0 > d1:
+        centers[i: j] = np.array((ankle, frontf, rearf))
+    return centers
+
+    
+def regions(centers, schema, r=1.0):
+    u"""Encuentra los centros de las regiones del esquema.
+    
+    Los marcadores se encuentran diagramados dentro de un esquema
+    segun regiones. Esta función devuelve el centro de región junto
+    con un radio. Estos dos valores se utilizan para delimitar un
+    entorno donde es posible encontrar los marcadores e identificarlos en
+    un grupo cuando el esquema no está completo, es decir, cuando
+    se pierden marcadores.
+    El valor que devuelve como centro es el valor centro de la región en
+    la coordenada "y", es decir la altura del cuadro donde se encuentra
+    la región.
+    El valor que devuelve como radio, es la distancia que existe entre
+    los marcadores de los extremos de la región, multiplicada por el valor
+    del argumento r.
+    
+    :param centers: arreglo que contiene los centros de los marcadores.
+    este arreglo es de rango completo, contienen el número de marcadores
+    que espera el esquema.
+    :type centers: np.ndarray
+    :param schema: es el vector que contiene la información de la cantidad de
+    marcadores que contiene cada región.
+    :type schema: tuple
+    :param r: es un escalar con el que se amplia el radio de la región.
+    :type r: float
+    :return: vectores con el centro y radio de cada una de las regiones
+    diagramadas en el esquema.
+    :rtype: list
+    """
+    regions = []
+    i = 0
+    # Se separa el arreglo de centros de marcadores según el esquema.
+    for s in schema:
+        roi = centers[i: i+s]
+        # Se toman los valores "y" de los extremos del conjunto.
+        # WARNING: Según este resultado, si el número generado
+        # por el conjunto es 1, entonces no existe radio para
+        # el entorno.
+        y1, y2 = roi[(0, -1), 1]
+        dh = (y2 - y1)
+        roi_center = y1 + dh*.5
+        regions.append((roi_center, dh*r))
+        i += s
+    return regions
+
+
+def filling(centers, roi, schema):
+    u"""Esta función completa el rango del arreglo de marcadores.
+    
+    Esta función se ejecuta cuando el rango de la matriz de marcadores
+    es menor que el esperado según el esquema. Se analiza el arreglo
+    por regiones, cuando en alguna de ellas existe menor cantidad de
+    datos, entonces la región se rellena con valores aleatorios. Los
+    valores de las regiones donde el número es el correcto se mantienen.
+    
+    :param centers: el arreglo de centro de marcadores.
+    :type centers: np.ndarray
+    :param roi: vector con valores de centro y radio de una región
+    para crear un entorno de búsqueda.
+    :type roi: list
+    :param schema: vector con el número de marcadores esperados por
+    región.
+    :type schema: tuple
+    :return: vector cuya primera componente es el arreglo de marcadores
+    con rango completo, y segunda componete una lista de esquema
+    incompleto.
+    :rtype: tuple
+    """
+    # Las variables que se utilizan para indexar el arreglo de centros
+    # marcadores. La variable j es secundaria a i, y se adapta a cuando
+    # existe una región donde faltan datos.
+    i, j = 0, 0
+    # La variable replace es una lista que contiene el valor del vector
+    # esquema al que le faltan datos.
+    replace = []
+    # el nuevo arreglo "vacio" que se devuelve para interpolar. Los
+    # espacios que que no son ocupados por la región orignal es
+    # basura, no fiarse de esos valores.
+    resized_centers = np.empty((sum(schema), 2), dtype=int)
+    for (c, r), s in zip(roi, schema):
+        # En el arreglo rmark están los centros de los marcadores
+        # que corresponden al entorno delimitado por la región.
+        upper_limit = centers[:, 1] > c-r
+        lower_limit = centers[:, 1] < c+r
+        rmark = centers[np.logical_and(upper_limit, lower_limit)]
+        if len(rmark) != s:
+            # Si el número de marcadores dentro de la región no es el
+            # esperado, entonces el arreglo queda "vacio" en ese lugar
+            # y se reduce el número necesario en la variable secundaria
+            # de indexación.
+            replace.append(s)
+            j = i - (s - len(rmark))
+        else:
+            # Los marcadores originales se mantienen.
+            resized_centers[i: i+s] = centers[j: j+s]
+        i += s
+        j += s
+    return (resized_centers, replace)
+
+
+# In[ ]:
 
 
 # OK!
 import os
 from collections import deque
 from contextlib import contextmanager
+
+# tengo que usar logging
+import logging
+logging.info('Encendiendo Motores')
+
 
 class KinematicsEngine(object):
     u"""."""
@@ -245,9 +348,16 @@ class KinematicsEngine(object):
         # marcadores es mas complejo, y se necesita contexto distinto
         # al de un archivo de texto.
         if ext in self.config.get('Sys', 'videoext').split():
-            manager = self.openVideo
-            process = self.VideoProcess
-        
+            with self.openVideo(self.filename) as stream:
+                # Primero se buscan las caminatas dentro del video. Se
+                # recorren todos los cuadros en búsqueda de caminatas.
+                # A este proceso le llamo slicing.
+                self.VideoProcess(stream, 'slice')
+                # Si se obtuvieron caminatas, entonces el proceso
+                # continua con la identificación y agrupación de los
+                # marcadores. A este proceso le llamo grouping.
+                self.VideoProcess(stream, 'group')
+
         # Si por el contrario el archivo es de texto, entonces se debe
         # comprobar que sea de kinovea, con el formato apropiado, y
         # el contexto de manejo y proceso tambien es distinto.
@@ -260,9 +370,7 @@ class KinematicsEngine(object):
             raise Exception(u"MasMarcha: Formato de archivo NO soportado")
 
         # Se inicia el proceso de obtención de datos.
-        with manager(self.filename) as stream:
-            process(stream)
-            process(stream, 'group', startFrame=self.hikes[0][0], endFrame=self.hikes[0][1])
+        # Esta parte no está clara del todo.
 
     def VideoProcess(self, buff, mode='slice', startFrame=0.0, endFrame=None):
         u"""Procesa el archivo de video que contiene la marcha.
@@ -305,7 +413,7 @@ class KinematicsEngine(object):
             raise Exception(u"Masmarcha: KinematicsEngine.VideoProcess(modo['slice' o 'group'])")
 
 
-# In[19]:
+# In[ ]:
 
 
 # main
@@ -335,12 +443,6 @@ K.hikes
 # In[ ]:
 
 
-
-
-
-# In[3]:
-
-
 # path = '/home/mariano/Descargas/VID_20170720_132629833.mp4'  # Belen
 
 # marks = []
@@ -362,4 +464,57 @@ K.hikes
 # #             print(np.linalg.norm(temp - mark, axis=1).round())
 # #         temp = mark
 #     ret, frame = vid.read()
+
+
+# In[4]:
+
+
+path = '/home/mariano/Descargas/VID_20170720_132629833.mp4'  # Belen
+vid = cv2.VideoCapture(path)
+for i in range(10):
+    ret, frame = vid.read()
+plt.imshow(frame)
+plt.show()
+
+centers = np.array(map(markerCenter, findMarkers(frame)))[::-1]
+centers
+
+
+# In[5]:
+
+
+ROI = regions(centers, schema['schema'], 1.2)
+for c, d in ROI:
+    plt.imshow(frame)
+    plt.axhline(y=c)
+    plt.axhline(y=c-d, color='r')
+    plt.axhline(y=c+d, color='r')
+    
+    plt.show()
+
+
+# In[6]:
+
+
+filling(centers, ROI, schema['schema'])
+
+
+# In[7]:
+
+
+modified = centers.copy()
+modified = modified[(0,1, 2, 3, 4, 6), :]
+modified
+
+
+# In[12]:
+
+
+filling(modified, ROI, schema['schema'])
+
+
+# In[10]:
+
+
+get_ipython().magic(u'pinfo np.ndarray')
 
