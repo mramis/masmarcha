524a21816c42a98a3630c47a69dbb61fb746c665
Mariano <ramismariano@gmail.com>
2018-05-07 13:55:54 -0300

    Se continua con el desarrollo de las gráficas aticulares.

--- src/representation.py
+++ src/representation.py
@@ -18,10 +18,13 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import matplotlib.pyplot as plt
-import matplotlib.style as style
+
+from string import capitalize
 from collections import defaultdict
 
+import matplotlib.pyplot as plt
+import matplotlib.style as style
+import numpy as np
 
 style.use('seaborn')
 
@@ -32,53 +35,67 @@ TABASICS = dict(
     cellLoc='left',
     colLoc='left',
     colLabels=['Fecha', 'Nombre', 'Lat', 'Prueba', 'Asistencia'],
-    colWidths=[0.1, 0.3, 0.1, 0.25, 0.25]
+    colWidths=[0.125, 0.275, 0.1, 0.25, 0.25])
+
+JOINTLIMITS = dict(
+    hip=(-30, 50),
+    knee=(-30, 90),
+    ankle=(-40, 30)
 )
 
 
-def basic_joint_plot(joint_name, joint_data, joint_meta, psize=(10, 7)):
+def basic_joint_plot(joint_name, joint_data, joint_meta, meanstd=None):
     u"""."""
+    # NOTE: hay que poner un límite de 10 sesiones.
 
-    # Note: hay que poner un límite de 10 sesiones.
+    # NOTE: esto se tiene que agregar a la configuración.
+    psize = (10, 7)
+    dpi = 80
 
-    # La figura por defecto tiene las dimensiones psize, y una resolución de
-    # 80 pixeles por pulgada, el título corresponde al nombre de la
-    # articulación.
-    # NOTE: Se tiene que ajustar el h_space a la cantidad de sessiones que se
-    # dibujan.
-    fig = plt.figure(figsize=psize, dpi=80)
-    fig.suptitle(joint_name)
-    fig.subplots_adjust(hspace=0.7)  # NOTE: (*)
+    fig = plt.figure(figsize=psize, dpi=dpi)
+    fig.suptitle(capitalize(joint_name), fontsize=24)
+    # NOTE: el hspace aumenta a medida que aumentan las sesiones.
+    # fig.subplots_adjust(hspace=0.7)
 
-    # EL primer sublot es el gráfico de la cinemática, y el segundo es la tabla
-    # con los datos del entorno de marcha (metadata)
+    # El primero es el axes en el que se dibujan las curvas de la articulación,
+    # el segundo es el de la tabla que contiene la leyenda.
     main_plot = plt.subplot2grid((3, 1), (0, 0), rowspan=2)
     meta_plot = plt.subplot2grid((3, 1), (2, 0))
 
-    # Este diccionario es el que se utiliza para construir las filas de la
-    # tabla. En la iteración se agregan los datos de la cantidad de ciclos que
-    # tiene cada sesión y el color con la que se dibuja la misma.
+    if meanstd:
+        # NOTE: Hay que ver si no es mejor que la línea de la media no esté
+        # encima del resto de las curvas.
+        main_plot.plot(meanstd[0], color='k', linewidth=1.5)
+        main_plot.fill_between(np.arange(meanstd[0].size),
+                               meanstd[0]-meanstd[1],
+                               meanstd[0]+meanstd[1],
+                               color='k', alpha=.3)
+    # Este diccionario se utiliza para la construcción de la tabla de metadata,
+    # durante la construcción de las gráficas se le agregan el tamaño de la
+    # muestra de sesión, y el color de la misma.
     tabcollection = defaultdict(list)
-
-    # Se dibujan las curvas de la cinemática de la articulación.
-    for c, data in enumerate(joint_data):
-        main_plot.plot(data, color=COLORMAP[c])
+    # Se construyen las curvas de la articulación, la curva va tomando mas
+    # transparencia a medida que pasan las sesiones (alpha).
+    alpha = .6
+    for c, session in enumerate(joint_data):
+        tabcollection['rowLabels'].append(len(session))
         tabcollection['rowColours'].append(COLORMAP[c])
-        tabcollection['rowLabels'].append(len(data))
-
-    # NOTE: Continuar desde acá con los detalles del subplot de cinemática.
+        for data in session:
+            main_plot.plot(data[0], data[1], color=COLORMAP[c], alpha=alpha)
+        alpha -= 0.04
+    # Se retocan los axis, y se agrega la línea de la posición neutra.
     main_plot.axhline(0, color='k', linestyle='--', linewidth=1.0)
-    main_plot.set_ylabel(u'Extensión    Flexión')
-    main_plot.set_xlabel(u'% Ciclo de marcha')
-
-    # Se agregan a la tabla los valores que completan las celdas, estos datos
-    # deben estár ordenados como lo están los datos de cinemática. Además se
-    # actualiza con los valores basicos.
+    main_plot.set_ylim(*JOINTLIMITS[joint_name])
+    main_plot.set_ylabel(u'Grados de rotación (-extensión, +flexión)')
+    main_plot.set_xlabel(u'[%] Ciclo de marcha')
+    # Se termina de construir el diccionario que se utiliza en la tabla. Se,
+    # agregan los valores de las celdas, y se actualiza con el diccionario que
+    # contiene los valores por defecto.
     tabcollection['cellText'] = joint_meta
     tabcollection.update(TABASICS)
-
-    # Se dibuja la tabla.
+    # Se construye la tabla.
     meta_plot.axis('off')
     meta_plot.table(**tabcollection)
 
     plt.show()
+    plt.close()
