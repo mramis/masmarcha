f9246f65a5757622be83a296c1eaf2c954bebcee
Mariano <ramismariano@gmail.com>
2018-05-10 19:43:47 -0300

    Se continua con el script de represantiones, se está probando el gráfico de articulaciones. Se agrega test_repr

--- src/representation.py
+++ src/representation.py
@@ -38,15 +38,32 @@ TABASICS = dict(
     colWidths=[0.125, 0.275, 0.1, 0.25, 0.25])
 
 JOINTLIMITS = dict(
-    hip=(-30, 50),
-    knee=(-30, 90),
-    ankle=(-40, 30)
+    cadera=(-30, 50),
+    rodilla=(-30, 90),
+    tobillo=(-40, 30)
 )
 
 
-def basic_joint_plot(joint_name, joint_data, joint_meta, meanstd=None):
+def basic_joint_plot(joint_name, joint_data, joint_meta, sac=None):
     u"""."""
-    # NOTE: hay que poner un límite de 10 sesiones.
+    data_lenght = len(joint_data)
+    if data_lenght > 10:
+        # Si la cantidad de sesiones es mayor a 10, para que la tabla que
+        # contiene la metadata no ocupe mas superficie que el gráfico, se
+        # recortan los datos en listas de diez.
+        if data_lenght % 10 == 0:
+            n_plots = data_lenght / 10
+        else:
+            n_plots = data_lenght / 10 + 1
+        # Aquí se produce la recursión de la función de dibujo.
+        i = 0
+        for j in xrange(n_plots):
+            basic_joint_plot('%s_%d' % (joint_name, j+1),
+                             joint_data[i: (j+1)*10],
+                             joint_meta[i: (j+1)*10],
+                             sac)
+            i += (j+1)*10
+        return
 
     # NOTE: esto se tiene que agregar a la configuración.
     psize = (10, 7)
@@ -54,26 +71,32 @@ def basic_joint_plot(joint_name, joint_data, joint_meta, meanstd=None):
 
     fig = plt.figure(figsize=psize, dpi=dpi)
     fig.suptitle(capitalize(joint_name), fontsize=24)
-    # NOTE: el hspace aumenta a medida que aumentan las sesiones.
-    # fig.subplots_adjust(hspace=0.7)
+    fig.subplots_adjust(hspace=0.12*data_lenght)
 
     # El primero es el axes en el que se dibujan las curvas de la articulación,
     # el segundo es el de la tabla que contiene la leyenda.
     main_plot = plt.subplot2grid((3, 1), (0, 0), rowspan=2)
     meta_plot = plt.subplot2grid((3, 1), (2, 0))
 
-    if meanstd:
-        # NOTE: Hay que ver si no es mejor que la línea de la media no esté
-        # encima del resto de las curvas.
-        main_plot.plot(meanstd[0], color='k', linewidth=1.5)
-        main_plot.fill_between(np.arange(meanstd[0].size),
-                               meanstd[0]-meanstd[1],
-                               meanstd[0]+meanstd[1],
-                               color='k', alpha=.3)
     # Este diccionario se utiliza para la construcción de la tabla de metadata,
     # durante la construcción de las gráficas se le agregan el tamaño de la
     # muestra de sesión, y el color de la misma.
     tabcollection = defaultdict(list)
+
+    if sac:
+        mean, std, nsac = sac
+        # NOTE: Hay que ver si no es mejor que la línea de la media no esté
+        # encima del resto de las curvas.
+        main_plot.plot(mean, color='k', linewidth=1.5)
+        main_plot.fill_between(np.arange(mean.size),
+                               mean-std,
+                               mean+std,
+                               color='k', alpha=.3)
+        # Se agregan a la tabla los datos de la media SAC
+        tabcollection['rowLabels'].append(nsac)
+        tabcollection['rowColours'].append((0., 0., 0.))
+        joint_meta.insert(0, ['-', 'SAC (N=%d)' % nsac, 'izq-der', '-', '-'])
+
     # Se construyen las curvas de la articulación, la curva va tomando mas
     # transparencia a medida que pasan las sesiones (alpha).
     alpha = .6
@@ -85,7 +108,7 @@ def basic_joint_plot(joint_name, joint_data, joint_meta, meanstd=None):
         alpha -= 0.04
     # Se retocan los axis, y se agrega la línea de la posición neutra.
     main_plot.axhline(0, color='k', linestyle='--', linewidth=1.0)
-    main_plot.set_ylim(*JOINTLIMITS[joint_name])
+    main_plot.set_ylim(*JOINTLIMITS[joint_name.split('_')[0]])
     main_plot.set_ylabel(u'Grados de rotación (-extensión, +flexión)')
     main_plot.set_xlabel(u'[%] Ciclo de marcha')
     # Se termina de construir el diccionario que se utiliza en la tabla. Se,
@@ -97,5 +120,5 @@ def basic_joint_plot(joint_name, joint_data, joint_meta, meanstd=None):
     meta_plot.axis('off')
     meta_plot.table(**tabcollection)
 
-    plt.show()
+    plt.show()  # Cambiar por plt.savefig()
     plt.close()
