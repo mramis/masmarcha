910644c03f58b524ab42da971538df5a53c528d2
Mariano Ramis <ramismariano@gmail.com>
2018-08-13 20:46:05 -0300

    Solo cambios de pruebas

--- src/preview.py
+++ src/preview.py
@@ -6,13 +6,13 @@ from json import load
 from video import play, draw_markers, draw_rois
 from engine import VideoEngine
 
-path = "/home/mariano/masmarcha/capturas/cata.mp4"
+path = "/home/mariano/Descargas/VID_20180813_182041747.mp4"
 
 if __name__ == '__main__':
     config = ConfigParser()
     config.read('/home/mariano/masmarcha/masmarcha.ini')
     schema = load(open(config.get('paths', 'schema')))
     extrapx = config.getint('engine', 'extrapixel')
-    play(path, 0.05, draw_markers, markers=None, wichone=None)
+    play(path, 0.0, draw_markers, markers=None, wichone=None)
 
-    # play(path, 0.05, draw_rois, regions=None, schema=schema, extrapx=extrapx)
+    # play(path, 0.005, draw_rois, regions=None, schema=schema, extrapx=extrapx)
