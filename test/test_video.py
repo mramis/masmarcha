9bb9eb60c0f7abb8be20ced3d8fe47d18cfa4fa5
Mariano Ramis <ramismariano@gmail.com>
2018-08-13 22:13:16 -0300

    Se testea la función de interpolar regiones

--- test/test_video.py
+++ test/test_video.py
@@ -26,6 +26,7 @@ from configparser import ConfigParser
 from io import StringIO
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 sys.path.insert(0, 'src')
 
@@ -43,79 +44,77 @@ config.read_file(StringIO(string_config))
 schema = load(open(config.get('paths', 'schema')))
 
 
-def test_openvideo():
-    path = "/home/mariano/masmarcha/capturas/calb-dist-tomi.mp4"
-    with video.open_video(path) as vid:
-        assert(isinstance(vid, video.cv2.VideoCapture))
+def test_interpolate():
+    # Las regiones son arreglos de dos vectores en el plano que apuntan a las
+    # esquinas de un rectángulo, en el que se encuentran los marcadores.
+    v11 = np.array(((1, 1), (3, 3)))
+    v21 = v11.copy()
+    v21[:, 1] += 3
+    v31 = v21.copy()
+    v31[:, 1] += 3
 
+    R1 = np.array((v11, v21, v31))
+    R2 = R1 + np.array((3, 0))
 
-def test_getfps():
-    path = "/home/mariano/masmarcha/capturas/calb-dist-tomi.mp4"
-    assert(isinstance(video.get_fps(path), float))
+    # Esta es la respuesta a la interpolacion con un punto intermedio.
+    vr = np.array((((2.5, 1), (4.5, 3)),
+    ((2.5, 4), (4.5, 6)),
+    ((2.5, 7), (4.5, 9)))).flatten()
 
+    R = video.interpolate_lost_regions(
+    (np.array((0, *R1.flatten())),
+    np.array((2, *R2.flatten()))),
+    schema)
 
-def test_contours():
-    path = "/home/mariano/masmarcha/capturas/calb-dist-tomi.mp4"
-    with video.open_video(path) as vid:
-        __, frame = vid.read()
-        contours = video.find_contours(frame, dilate=True)
-        center =  video.contour_center(contours[0])
-        array_of_centers = video.contour_centers_array(contours)
+    assert(all(R[0, 1:] == vr))
 
+    # plt.plot(*v11.T, 'b-')
+    # plt.plot(*v21.T, 'r-')
+    # plt.plot(*v31.T, 'g-')
 
-def test_distance():
-    u"""."""
-    path = "/home/mariano/masmarcha/capturas/calb-dist-tomi.mp4"
-    assert(isinstance(video.get_distance_scale(path, 0.3), float))
+    # v12, v22, v32 = R2
+    # plt.plot(*v12.T, 'b-')
+    # plt.plot(*v22.T, 'r-')
+    # plt.plot(*v32.T, 'g-')
 
+    # v1, v2, v3 = R[0, 1:].reshape(3, 2, 2)
+    # plt.plot(*v1.T, 'b--')
+    # plt.plot(*v2.T, 'r--')
+    # plt.plot(*v3.T, 'g--')
 
-def test_calibrate():
-    path = '/home/mariano/masmarcha/capturas/damero.mp4'
-    video.calibrate_camera(path, os.path.join('./test','MOTOG3'), (8, 4), 10,
-                           config)
-
-def test_walks():
-    pass
-
-
-# def test_getrois():
-#     arr = np.ndarray((7, 2))
-#     fi = 10
-#     assert(get_regions(fi, arr, schema).size == len(schema['schema']*2*2) + 1)
-#
-#
-# def test_interprois():
-#     pre = np.hstack((0, np.arange(12)))
-#     cur = np.hstack((10, np.arange(12, 24)))
-#     dom = np.arange(pre[0] + 1, cur[0])
-#     arr = interpolate_lost_regions((pre, cur), schema)
-#     assert(arr.shape[0] == dom.size)
+    # plt.savefig('test/interpolate')
 
 
 
-#
-#
-# def test_markers():
-#     with open_video(path) as video:
-#         video.set(cv2.CAP_PROP_POS_FRAMES, 100)
-#         ret, frame = video.read()
-#         assert(ret == True)
-#         assert(isinstance(frame, np.ndarray))
-#         # testintg find_contours
-#         contours = find_contours(frame)
-#         assert(isinstance(contours, list))
-#         # testing markers_center
-#         center = marker_center(contours[0])
-#         assert(isinstance(center, tuple))
-#         # testing get_centers
-#         markers = get_markers(frame)
-#         assert(isinstance(markers, np.ndarray))
-#
-#
-# def test_explorer():
-#     walks = find_walks(path, config)
-#     for i, interval in enumerate(walks):
-#         assert(isinstance(interval, tuple))
-#         assert(len(interval) == 2)
-#         if i == 1:
-#             walks.close()
+
+
+# def test_calibrate():
+#     path = '/home/mariano/masmarcha/capturas/damero.mp4'
+#     video.calibrate_camera(path, os.path.join('./test','MOTOG3'), (8, 4), 10,
+#                            config)
+
+
+# def test_distance():
+#     u"""."""
+#     path = "/home/mariano/masmarcha/capturas/calb-dist-tomi.mp4"
+#     assert(isinstance(video.get_distance_scale(path, 0.3), float))
+
+
+# def test_contours():
+#     path = "/home/mariano/masmarcha/capturas/calb-dist-tomi.mp4"
+#     with video.open_video(path) as vid:
+#         __, frame = vid.read()
+#         contours = video.find_contours(frame, dilate=True)
+#         center =  video.contour_center(contours[0])
+#         array_of_centers = video.contour_centers_array(contours)
+
+
+# def test_getfps():
+#     path = "/home/mariano/masmarcha/capturas/calb-dist-tomi.mp4"
+#     assert(isinstance(video.get_fps(path), float))
+
+
+# def test_openvideo():
+#     path = "/home/mariano/masmarcha/capturas/calb-dist-tomi.mp4"
+#     with video.open_video(path) as vid:
+#         assert(isinstance(vid, video.cv2.VideoCapture))
